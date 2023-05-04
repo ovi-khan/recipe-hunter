c1467f46357447a2262c5f424434a1f1cb7b4ecc
ovi-khan <manikkhan2240@gmail.com>
2023-05-04 20:15:18 +0600

    environment variable setup firebase auth

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('environment variable', import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyBqp53wdBZTOJbTX9QRt0ughTsMgCZW4No",
-  authDomain: "recipie-hunter.firebaseapp.com",
-  projectId: "recipie-hunter",
-  storageBucket: "recipie-hunter.appspot.com",
-  messagingSenderId: "640341303733",
-  appId: "1:640341303733:web:78be001e67803e347439fc"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -68,3 +68,87 @@ const Header = () => {
 };
 
 export default Header;
+
+
+// import React, { useState } from 'react';
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+// import ActiveLink from '../ActiveLink/ActiveLink';
+// import logoimg from "../../../../public/images/logoimg/thaiFoodLogo.jpg";
+// import { Link } from "react-router-dom";
+// import { AuthContext } from "../../../provider/AuthProvider";
+// import ActiveLink from "../ActiveLink/ActiveLink";
+
+// const Header = () => {
+//     const [open, setOpen] = useState(false)
+
+// const { user, logOut } = useContext(AuthContext);
+// // console.log(user?.email)
+// const handleLogOut = () => {
+//   logOut()
+//     .then()
+//     .catch((error) => {
+//       console.log(error.message);
+//     });
+// };
+    
+    
+//     return (
+//         <nav className='mt-5 mx-10'>
+//             <div className='md:hidden' onClick={() => setOpen(!open)}>
+//                 <span>{open == true?<XMarkIcon className="h-6 w-6 text-blue-500" />: <Bars3Icon className="h-6 w-6 text-blue-500" />}</span>
+            
+            
+//             </div>
+//             <ul className={`md:flex gap-5 duration-1000 absolute md:static bg-slate-300 p-5 ${open ? 'left-14': '-left-48'}`}>
+//             <Link to="/" className="normal-case text-xl">
+//            <img
+//               className="h-14 w-20 object-cover rounded-lg border border-red-500"
+//               src={logoimg}
+//               alt=""
+//               />
+//           </Link>
+//             </ul>
+//         <div className="gap-5">
+//           <div className="gap-10 lg:flex">
+//             <ActiveLink className="ms-5" to="/">
+//               Home
+//             </ActiveLink>
+//             <ActiveLink className="ms-5" to="/chefDetails">
+//               Chef Details
+//             </ActiveLink>
+//             <ActiveLink className="ms-5" to="/blog">
+//               Blog
+//             </ActiveLink>
+//             <ActiveLink className="ms-5" to="/signUp">
+//               Sign Up
+//             </ActiveLink>
+//             {user?.email ? (
+//               <button
+//                 onClick={handleLogOut}
+//                 className="bg-slate-500 px-3 py-1 rounded-lg"
+//               >
+//                 Log Out
+//               </button>
+//             ) : (
+//               <ActiveLink className="ms-5" to="/login">
+//                 Log in
+//               </ActiveLink>
+//             )}
+//             {/* <li>{user?.email}</li>  */}
+//           <div className="">
+//             {user && (
+//               <label className="btn btn-ghost btn-circle avatar">
+//                 <div className="w-10 rounded-full">
+//                   <img className="object-cover" src={user?.photoURL} />
+//                 </div>
+//               </label>
+//             )}
+//           </div>
+//            </div>
+//             </div>
+            
+//         </nav>
+//     );
+// };
+
+// export default Header;
