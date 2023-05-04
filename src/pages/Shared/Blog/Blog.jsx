2596e4555d98447d40d7b8adba0bb786c1eea78c
ovi-khan <manikkhan2240@gmail.com>
2023-05-04 18:13:21 +0600

    blog section question and answire

--- src/pages/Shared/Blog/Blog.jsx
+++ src/pages/Shared/Blog/Blog.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            <h1>This is the blog page</h1>  
-        </div>
-    );
+  return (
+    <div className="mx-20 my-20">
+      <h2 className="text-2xl text-red-600 font-semibold mb-2">
+        1. Tell us the differences between uncontrolled and controlled
+        components?
+      </h2>
+      <p className="text-green-900 mb-5">
+        managed internally by the component itself. The component handles its
+        own state, and updates to the component's state are not reflected
+        outside of the component. For example, a form input element such as is
+        an uncontrolled component because its value is managed by the DOM, not
+        by React. When a user types into the input field, the DOM updates its
+        own value property, and the React component does not have access to this
+        value until it is submitted.
+      </p>
+      <h2 className="text-2xl text-red-600 font-semibold mb-2">
+        2. How to validate React props using PropTypes?
+      </h2>
+      <p className="text-green-900 mb-5">
+        React provides a utility called PropTypes for validating the types of
+        props passed to a component. It helps to ensure that the correct types
+        of data are being passed as props, which can catch bugs early in the
+        development process. To use PropTypes, you need to import it from the
+        prop-types package. When a prop fails validation, React will print a
+        console warning with a detailed message about the error. This can help
+        you to catch and fix errors early in the development process.
+      </p>
+      <h2 className="text-2xl text-red-600 font-semibold mb-2">
+        3. Tell us the difference between nodejs and express js?
+      </h2>
+      <p className="text-green-900 mb-5">
+        Node.js is a server-side JavaScript runtime that allows developers to
+        build server-side applications in JavaScript. It is built on the Chrome
+        V8 engine and provides a platform for building scalable,
+        high-performance applications. Express.js, on the other hand, is a
+        popular web application framework for Node.js that simplifies the
+        development of web applications by providing a set of robust features
+        and tools. Express.js is built on top of Node.js and provides a
+        higher-level abstraction for building web applications, making it easier
+        to build web servers, APIs, and web applications.
+      </p>
+      <h2 className="text-2xl text-red-600 font-semibold mb-2">
+        4. What is a custom hook, and why will you create a custom hook?
+      </h2>
+      <p className="text-green-900 mb-5">
+        A custom hook can be created for any logic that needs to be reused, such
+        as fetching data from an API, handling form input, managing state, and
+        more. By creating a custom hook, developers can abstract away complex
+        logic and reuse it in multiple components, reducing the amount of code
+        they need to write and maintain. Custom hooks can also improve the
+        readability and organization of code by separating concerns and making
+        it easier to understand what each component is responsible for. They can
+        also make testing easier by allowing developers to test logic in
+        isolation.
+      </p>
+    </div>
+  );
 };
 
-export default Blog;    
+export default Blog;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,49 +1,69 @@
 import React, { useContext } from "react";
-import logoimg from "../../../../public/images/logoimg/thaiFoodLogo.jpg"
+import logoimg from "../../../../public/images/logoimg/thaiFoodLogo.jpg";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../../provider/AuthProvider"
+import { AuthContext } from "../../../provider/AuthProvider";
 import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
-  const { user, logOut } = useContext(AuthContext)
+  const { user, logOut } = useContext(AuthContext);
   // console.log(user?.email)
   const handleLogOut = () => {
     logOut()
-    .then()
-    .catch(error => {
-      console.log(error.message)
-    })
-  }
+      .then()
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
-    <div className="navbar bg-base-100 bg-orange-500 px-8">
-      <div className="flex-1">
-        <Link to='/' className="normal-case text-xl">
-            <img className="h-14 w-20 object-cover rounded-lg border border-red-500" src={logoimg} alt="" />
-        </Link>
-      </div>
-      <div className="gap-5">
-        <ul className="gap-10 flex flex-row">
-        <ActiveLink className="ms-5" to='/'>Home</ActiveLink>
-        <ActiveLink className="ms-5" to='/chefDetails'>Chef Details</ActiveLink>
-            <ActiveLink className="ms-5" to='/signUp'>Sign Up</ActiveLink>
-            { user?.email ? (
-                 <button onClick={handleLogOut} className="btn">Log Out</button>
-            ):(
-                <ActiveLink className="ms-5" to='/login'>Log in</ActiveLink>
-              )}
-              {/* <li>{user?.email}</li>  */}
-            <ActiveLink className="ms-5" to='/blog'>Blog</ActiveLink>
-        </ul>
-        <div className="">
-         { user && <label  className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img className="object-cover" src={user?.photoURL} />
-            </div>
-          </label>}
-          
+      <div className="lg:flex justify-between justify-end items-center bg-base-100 bg-orange-500 px-8">
+        <div className="lg:flex">
+          <Link to="/" className="normal-case text-xl">
+            <img
+              className="h-14 w-20 object-cover rounded-lg border border-red-500"
+              src={logoimg}
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="gap-5">
+          <div className="gap-10 lg:flex">
+            <ActiveLink className="ms-5" to="/">
+              Home
+            </ActiveLink>
+            <ActiveLink className="ms-5" to="/chefDetails">
+              Chef Details
+            </ActiveLink>
+            <ActiveLink className="ms-5" to="/blog">
+              Blog
+            </ActiveLink>
+            <ActiveLink className="ms-5" to="/signUp">
+              Sign Up
+            </ActiveLink>
+            {user?.email ? (
+              <button
+                onClick={handleLogOut}
+                className="bg-slate-500 px-3 py-1 rounded-lg"
+              >
+                Log Out
+              </button>
+            ) : (
+              <ActiveLink className="ms-5" to="/login">
+                Log in
+              </ActiveLink>
+            )}
+            {/* <li>{user?.email}</li>  */}
+          <div className="">
+            {user && (
+              <label className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img className="object-cover" src={user?.photoURL} />
+                </div>
+              </label>
+            )}
+          </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
