f783e8057a8748ae1254b0a53ccb2260a4bcc26a
ovi-khan <manikkhan2240@gmail.com>
2023-05-04 23:08:08 +0600

    our service done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from 'react-router-dom';
 import Banner from '../../Shared/Banner/Banner';
 import ChefCard from '../../Shared/ChefCard/ChefCard';
 import ChefStatus from '../../Shared/ChefStatus/ChefStatus';
+import OurServices from '../../Shared/OurServices/OurServices';
 
 const Home = () => {
     const navigation = useNavigation()
@@ -11,6 +12,7 @@ const Home = () => {
             <div className='text-4xl'>{navigation.state === 'loading' && 'Loading...'}</div>
             <Banner></Banner>
             <ChefStatus></ChefStatus>
+            <OurServices></OurServices>
             <ChefCard></ChefCard>
         </div>
     );
